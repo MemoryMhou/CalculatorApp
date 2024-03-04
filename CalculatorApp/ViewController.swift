de71079fde4f9fa23acf18957810058d4a0b4819
MemoryMhou <memorymhou1@gmail.com>
2024-03-04 20:54:10 +0200

    initial

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var OutPutLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    @IBAction func NumberPressed(_ sender: RoundButton) {
+    }
+    
 
+    @IBAction func allClearPressed(_ sender: RoundButton) {
+    }
+    
+    
+    @IBAction func dotPressed(_ sender: RoundButton) {
+    }
+    
+    
+    @IBAction func equalPressed(_ sender: RoundButton) {
+    }
+    
+    
+    @IBAction func addPressed(_ sender: RoundButton) {
+    }
+    
+    
+    @IBAction func subtractPressed(_ sender: RoundButton) {
+    }
+    
+    
+    @IBAction func multiplyPressed(_ sender: RoundButton) {
+    }
+    
+  
+    @IBAction func dividePressed(_ sender: RoundButton) {
+    }
+    
+    @IBAction func percentagePressed(_ sender: RoundButton) {
+    }
+    
+    
+    @IBAction func PlusOrMinusPressed(_ sender: RoundButton) {
+    }
+    
+    
 }
 
